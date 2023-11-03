98cc0cc16274101d894d61aa29da3b921926946d
riamems <membagloria@gmail.com>
2023-11-02 23:42:16 -0700

    modules python work

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -5,12 +5,7 @@ from calculator_1 import add, sub, mul, div
 a = 10
 b = 5
 
-result_add = add(a, b)
-result_sub = sub(a, b)
-result_mul = mul(a, b)
-result_div = div(a, b)
-
-print(result_add)
-print(result_sub)
-print(result_mul)
-print(result_div)
+  print("{:d} + {:d} = {:d}".format(a, b, add(a, b)))
+    print("{:d} - {:d} = {:d}".format(a, b, sub(a, b)))
+    print("{:d} * {:d} = {:d}".format(a, b, mul(a, b)))
+    print("{:d} / {:d} = {:d}".format(a, b, div(a, b)))
