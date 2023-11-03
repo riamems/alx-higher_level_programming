e88671e7f007e68885635fc44c28407632626f74
riamems <membagloria@gmail.com>
2023-11-02 22:32:25 -0700

    import modules work

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -1,11 +1,14 @@
 #!/usr/bin/python3
 
 if __name__ == "__main__":
-        from calculator_1 import add, sub, mul, div
-            a = 10
-                b = 5
-
-                    print("{:d} + {:d} = {:d}".format(a, b, add(a, b)))
-                        print("{:d} - {:d} = {:d}".format(a, b, sub(a, b)))
-                            print("{:d} * {:d} = {:d}".format(a, b, mul(a, b)))
-                                print("{:d} / {:d} = {:d}".format(a, b, div(a, b)))
+from calculator_1 import add, subtract, multiply, divide
+a = 10
+b = 5
+result_add = add(a, b)
+result_subtract = subtract(a, b)
+result_multiply = multiply(a, b)
+result_divide = divide(a, b)
+print(f"Adding {a} and {b} equals {result_add}")
+print(f"Subtracting {b} from {a} equals {result_subtract}")
+print(f"Multiplying {a} and {b} equals {result_multiply}")
+print(f"Dividing {a} by {b} equals {result_divide}")`
