9d721d3d10d090f600e21e7937eaa48b8ff87a37
riamems <membagloria@gmail.com>
2023-11-06 01:52:54 -0800

    data structures

--- 0x03-python-data_structures/0-print_list_integer.py
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -1,4 +1,8 @@
 #!/usr/bin/python3
 def print_list_integer(my_list=[]):
 for n in my_list:
-print("{:d}".format(n))
+print("{:d}".format(n)
+
+if __name__ == "__main__":
+my_list = [1, 2, 3, 4, 5]
+print_list_integer(my_list)
