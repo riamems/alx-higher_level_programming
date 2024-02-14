a556e67283412a317e603ebc8fcdccd40af6b41b
riamems <membagloria@gmail.com>
2024-02-14 00:45:19 -0800

    correction

--- 0x12-javascript-warm_up/12-object.js
+++ 0x12-javascript-warm_up/12-object.js
@@ -5,11 +5,4 @@ const myObject = {
 };
 console.log(myObject);
 myObject.value = 89;
-console.log(myObject);#!/usr/bin/node
-const myObject = {
-  type: 'object',
-  value: 12
-};
-console.log(myObject);
-myObject.value = 89;
 console.log(myObject);
